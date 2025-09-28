152574874e03129db318ddd26cf876333c668391
MarouLabs <marou.labs@proton.me>
2025-09-28 00:17:10 +0000

    fixup

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config:
     DEBUG = os.getenv("DEBUG", "False").lower() == "true"
     HOST = os.getenv("HOST", "127.0.0.1")
     PORT = int(os.getenv("PORT", "5000"))
+    COURSES_ROOT_DIRECTORY_ABS_PATH = os.getenv("COURSES_ROOT_DIRECTORY_ABS_PATH", "")
